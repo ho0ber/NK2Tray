6cbfc165b6f3360efa0201100e0e5c86868a7465
Samuel Colburn <ho0ber@gmail.com>
2019-01-12 16:09:27 -0500

    Further Tweaks
    Doop de doop

--- NK2Tray/Program.cs
+++ NK2Tray/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using NAudio.Midi;
@@ -17,12 +18,24 @@ namespace NK2Tray
         private int SpotifyPID;
         private int DiscordPID;
 
+        public struct Assignment
+        {
+            public String process;
+            public String windowName;
+            public int pid;
+
+            public Assignment(String p, String wn, int inPid)
+            {
+                process = p;
+                windowName = wn;
+                pid = inPid;
+            }
+        }
+
+        public Assignment[] assignments = new Assignment[8];
+
         public SysTrayApp()
         {
-            // Create a simple tray menu with only one item.
-            trayMenu = new ContextMenu();
-            trayMenu.MenuItems.Add("Exit", OnExit);
-
             // Create a tray icon. In this example we use a
             // standard system icon for simplicity, but you
             // can of course use your own custom icon too.
@@ -31,10 +44,73 @@ namespace NK2Tray
             trayIcon.Icon = new Icon(SystemIcons.Application, 40, 40);
 
             // Add menu to tray icon and show it.
-            trayIcon.ContextMenu = trayMenu;
+            trayIcon.ContextMenu = new ContextMenu();
+            trayIcon.ContextMenu.Popup += OnPopup; // (sender, args) => Console.WriteLine("Opening #2");
+            //trayIcon.Click += OnClick;
             trayIcon.Visible = true;
             BeginListening();
         }
+
+        private void OnPopup(object sender, EventArgs e)
+        {
+            ContextMenu trayMenu = (ContextMenu)sender;
+            trayMenu.MenuItems.Clear();
+            trayMenu.MenuItems.Add("Exit", OnExit);
+
+            foreach (AudioSession session in AudioUtilities.GetAllSessions())
+            {
+                if (session.Process != null)
+                {
+                    // only the one associated with a defined process
+                    Console.WriteLine(session.Process.ProcessName);
+                    Console.WriteLine(session.Process.MainWindowTitle);
+                    Console.WriteLine("");
+                    //String.Format("{0} - {1}", session.Process.ProcessName, session.Process.MainWindowTitle)
+                    MenuItem mi = new MenuItem(session.Process.ProcessName);
+                    foreach (var i in Enumerable.Range(0, 8))
+                    {
+                        MenuItem si = new MenuItem(String.Format("Fader {0}", i), AssignFader);
+                        si.Tag = new object[] { session.Process.ProcessName, session.Process.MainWindowTitle, session.Process.Id,  i };
+                        mi.MenuItems.Add(si);
+                    }
+                    trayMenu.MenuItems.Add(mi);
+                }
+            }
+            //Console.WriteLine(e);
+        }
+
+        private void AssignFader(object sender, EventArgs e)
+        {
+            String proc = (String)((object[])((MenuItem)sender).Tag)[0];
+            String name = (String)((object[])((MenuItem)sender).Tag)[1];
+            int pid = (int)((object[])((MenuItem)sender).Tag)[2];
+            int fader = (int)((object[])((MenuItem)sender).Tag)[3];
+            Console.WriteLine(String.Format("Assigning fader {0} to {1} - {2}", fader, proc, name));
+            assignments[fader] = new Assignment(proc, name, pid);
+        }
+
+        public ContextMenu BuildMenu()
+        {
+            // Create a simple tray menu with only one item.
+            trayMenu = new ContextMenu();
+            trayMenu.MenuItems.Add("Exit", OnExit);
+
+            foreach (AudioSession session in AudioUtilities.GetAllSessions())
+            {
+                if (session.Process != null)
+                {
+                    // only the one associated with a defined process
+                    Console.WriteLine(session.Process.ProcessName);
+                    Console.WriteLine(session.Process.MainWindowTitle);
+                    Console.WriteLine("");
+                    trayMenu.MenuItems.Add(String.Format("{0} - {1}", session.Process.ProcessName, session.Process.MainWindowTitle), OnExit);
+                }
+            }
+
+
+            return trayMenu;
+        }
+
 
         protected override void OnLoad(EventArgs e)
         {
@@ -87,18 +163,42 @@ namespace NK2Tray
             if (e.MidiEvent.CommandCode == MidiCommandCode.ControlChange)
             {
                 ControlChangeEvent me = (ControlChangeEvent)e.MidiEvent;
+                /*
                 //Console.WriteLine(String.Format("Channel = {0}  CC = {1}  other = {2}", me.Channel, me.CommandCode, me.GetType()));
-                if (me.Channel == 1 && (int)me.Controller == 7)
+                if (me.Channel == 1 && (int)me.Controller == 0)
+                {
+                    Console.WriteLine(String.Format("Found matching events: {0} {1}", (int)me.Controller, me.ControllerValue));
+                    if (me.ControllerValue % 3 == 0 || me.ControllerValue == 127)
+                        VolumeMixer.SetApplicationVolume(GetForegroundPID(), me.ControllerValue / 128f * 100f);
+                }
+                else if (me.Channel == 1 && (int)me.Controller == 7)
                 {
                     //Console.WriteLine(String.Format("Found matching events: {0} {1}", (int)me.Controller, me.ControllerValue));
                     if (me.ControllerValue % 3 == 0 || me.ControllerValue == 127)
                         VolumeMixer.SetApplicationVolume(SpotifyPID, me.ControllerValue / 128f * 100f);
                 }
-                if (me.Channel == 1 && (int)me.Controller == 6)
+                else if (me.Channel == 1 && (int)me.Controller == 6)
                 {
                     //Console.WriteLine(String.Format("Found matching events: {0} {1}", (int)me.Controller, me.ControllerValue));
                     if (me.ControllerValue % 3 == 0 || me.ControllerValue == 127)
                         VolumeMixer.SetApplicationVolume(DiscordPID, me.ControllerValue / 128f * 100f);
+                }
+                */
+                if (me.Channel == 1)
+                {
+                    Console.Write("Fader event; ");
+                    int fader = (int)me.Controller;
+                    if (assignments[fader].process != null)
+                    {
+                        Console.Write("Got assignment " + fader + "; " + assignments[fader].windowName + "; ");
+                        //int pid = GetPidByName(assignments[fader].windowName);
+                        if (true) //(me.ControllerValue % 3 == 0 || me.ControllerValue == 127)
+                        {
+                            VolumeMixer.SetApplicationVolume(assignments[fader].pid, me.ControllerValue / 128f * 100f);
+                            Console.Write("changed vol");
+                        }
+                    }
+                    Console.WriteLine();
                 }
             }
         }
@@ -106,8 +206,43 @@ namespace NK2Tray
         [DllImport("user32.dll")]
         public static extern IntPtr FindWindow(string strClassName, string strWindowName);
 
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
         [DllImport("user32.dll", SetLastError = true)]
         public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
+
+        public int GetPidByName(string name)
+        {
+            {
+                var hWnd = FindWindow(name, "");
+                if (hWnd == IntPtr.Zero)
+                    return 0;
+
+                uint pID;
+                GetWindowThreadProcessId(hWnd, out pID);
+                if (pID == 0)
+                    return 0;
+
+                Console.WriteLine(pID);
+                return Convert.ToInt32(pID);
+            }
+
+        }
+
+        public int GetForegroundPID()
+        {
+            var hWnd = GetForegroundWindow();
+            if (hWnd == IntPtr.Zero)
+                return 0;
+
+            uint pID;
+            GetWindowThreadProcessId(hWnd, out pID);
+            if (pID == 0)
+                return 0;
+
+            return Convert.ToInt32(pID);
+        }
 
         public int GetSpotifyPID()
         {
