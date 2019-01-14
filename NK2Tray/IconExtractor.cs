4005303aab14fa738dc5d5eb6cbc048290d3bde2
Samuel Colburn <ho0ber@gmail.com>
2019-01-13 19:07:30 -0500

    First pass at using NAudio for everything

---
+++ NK2Tray/IconExtractor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace NK2Tray
+{
+    public class IconExtractor
+    {
+        public static Icon Extract(string file, int number, bool largeIcon)
+        {
+            IntPtr large;
+            IntPtr small;
+            ExtractIconEx(file, number, out large, out small, 1);
+            try
+            {
+                return Icon.FromHandle(largeIcon ? large : small);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        [DllImport("Shell32.dll")]
+        private static extern int ExtractIconEx(string sFile, int iIndex, out IntPtr piLargeVersion, out IntPtr piSmallVersion, int amountIcons);
+    }
+}
--- NK2Tray/Program.cs
+++ NK2Tray/Program.cs
@@ -5,6 +5,7 @@ using System.Windows.Forms;
 using NAudio.Midi;
 using System.Collections.Generic;
 using NAudio.CoreAudioApi;
+using System.Diagnostics;
 
 namespace NK2Tray
 {
@@ -16,18 +17,24 @@ namespace NK2Tray
 
     public struct Assignment
     {
-        public String process;
+        public String processName;
         public String windowName;
         public int pid;
+        public String sessionIdentifier;
+        public String instanceIdentifier;
         public AssignmentType aType;
+        public AudioSessionControl audioSession;
 
 
-        public Assignment(String p, String wn, int inPid, AssignmentType at)
+        public Assignment(String p, String wn, int inPid, AssignmentType at, String sid, String iid, AudioSessionControl audsess)
         {
-            process = p;
+            processName = p;
             windowName = wn;
             pid = inPid;
             aType = at;
+            sessionIdentifier = sid;
+            instanceIdentifier = iid;
+            audioSession = audsess;
         }
     }
 
@@ -39,6 +46,8 @@ namespace NK2Tray
         private NotifyIcon trayIcon;
         private MidiIn midiIn;
         public Assignment[] assignments = new Assignment[8];
+        private MMDevice device;
+        //private readonly AudioSessionControl session;
 
         public SysTrayApp()
         {
@@ -52,11 +61,125 @@ namespace NK2Tray
             trayIcon.Visible = true;
             ListenForMidi();
 
+            
             // dump all audio devices
             //Console.WriteLine("Current Volume:" + defaultPlaybackDevice.Volume);
             //VolTest();
         }
 
+        private bool ProcessExists(uint processId)
+        {
+            try
+            {
+                var process = Process.GetProcessById((int)processId);
+                return true;
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+        }
+
+        private void DumpProps(AudioSessionControl session)
+        {
+            Process process = Process.GetProcessById((int)session.GetProcessID);
+            Console.WriteLine("=================================");
+            Console.WriteLine(String.Format("DisplayName = {0}", session.DisplayName));
+            Console.WriteLine(String.Format("AudioMeterInformation = {0}", session.AudioMeterInformation.PeakValues));
+            Console.WriteLine(String.Format("IconPath = {0}", session.IconPath));
+            Console.WriteLine(String.Format("GetSessionIdentifier = {0}", session.GetSessionIdentifier));
+            Console.WriteLine(String.Format("GetSessionInstanceIdentifier = {0}", session.GetSessionInstanceIdentifier));
+            Console.WriteLine(String.Format("IsSystemSoundsSession = {0}", session.IsSystemSoundsSession));
+            Console.WriteLine(String.Format("SimpleAudioVolume = {0}", session.SimpleAudioVolume.Volume));
+            Console.WriteLine(String.Format("GetProcessID = {0}", session.GetProcessID));
+            Console.WriteLine(String.Format("State = {0}", session.State));
+
+            Console.WriteLine(String.Format("lbl = {0}", process.MainWindowTitle != "" ? process.MainWindowTitle : process.ProcessName));
+
+        }
+
+        private void OnPopup(object sender, EventArgs e)
+        {
+            ContextMenu trayMenu = (ContextMenu)sender;
+            trayMenu.MenuItems.Clear();
+
+
+            // Make all of the fader menu items
+            foreach (var i in Enumerable.Range(0, 8))
+            {
+                MenuItem faderMenu = new MenuItem(String.Format("Fader {0} - {1}", i, assignments[i].processName));
+                trayMenu.MenuItems.Add(faderMenu);
+            }
+
+            // Add audio devices to each fader menu items
+            var deviceEnumerator = new MMDeviceEnumerator();
+            device = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+
+            GenerateDeviceMenuItems(ref trayMenu);
+
+            var sessions = device.AudioSessionManager.Sessions;
+            if (sessions != null)
+            {
+                //AppVolumePanels = new List<VolumePanel>(sessions.Count);
+                for (int i = 0; i < sessions.Count; i++)
+                {
+                    var session = sessions[i];
+                    if (session.IsSystemSoundsSession && ProcessExists(session.GetProcessID))
+                    {
+                        //Console.WriteLine(session.DisplayName);
+                        //AddVolumePanel(session);
+                        DumpProps(session);
+                        GenerateMenuItems(ref trayMenu, session);
+                        break;
+                    }
+                }
+                for (int i = 0; i < sessions.Count; i++)
+                {
+                    var session = sessions[i];
+                    if (!session.IsSystemSoundsSession && ProcessExists(session.GetProcessID))
+                    {
+                        //AddVolumePanel(session);
+                        //Console.WriteLine(session.DisplayName);
+                        DumpProps(session);
+                        GenerateMenuItems(ref trayMenu, session);
+                    }
+
+                }
+            }
+        }
+
+        private void GenerateMenuItems(ref ContextMenu trayMenu, AudioSessionControl session)
+        {
+            Process process = Process.GetProcessById((int)session.GetProcessID);
+            foreach (var i in Enumerable.Range(0, 8))
+            {
+                String sessionTitle;
+                if (session.IsSystemSoundsSession)
+                    sessionTitle = "System Sounds";
+                else if (process.MainWindowTitle == "")
+                    sessionTitle = process.ProcessName;
+                else
+                    sessionTitle = process.MainWindowTitle;
+
+                MenuItem si = new MenuItem(sessionTitle, AssignFader);
+                // Tag is unpacked in AssignFader - using this to plumb through
+                si.Tag = new object[] { process.ProcessName, process.MainWindowTitle, (int)session.GetProcessID, i, session.GetSessionIdentifier, session.GetSessionInstanceIdentifier, session };
+                trayMenu.MenuItems[i].MenuItems.Add(si);
+            }
+        }
+
+        private void GenerateDeviceMenuItems(ref ContextMenu trayMenu)
+        {
+            foreach (var i in Enumerable.Range(0, 8))
+            {
+                MenuItem si = new MenuItem("Master Volume", AssignFader);
+                // Tag is unpacked in AssignFader - using this to plumb through
+                si.Tag = new object[] { process.ProcessName, process.MainWindowTitle, (int)session.GetProcessID, i, session.GetSessionIdentifier, session.GetSessionInstanceIdentifier, session };
+                trayMenu.MenuItems[i].MenuItems.Add(si);
+            }
+        }
+
+        /*
         private void OnPopup(object sender, EventArgs e)
         {
             ContextMenu trayMenu = (ContextMenu)sender;
@@ -145,7 +268,7 @@ namespace NK2Tray
 
             return duplicates;
         }
-
+        */
         private void AssignFader(object sender, EventArgs e)
         {
             String proc = (String)((object[])((MenuItem)sender).Tag)[0];
@@ -153,11 +276,14 @@ namespace NK2Tray
             int pid = (int)((object[])((MenuItem)sender).Tag)[2];
             int fader = (int)((object[])((MenuItem)sender).Tag)[3];
             AssignmentType aType = pid >= 0 ? AssignmentType.Process : AssignmentType.Master;
+            String sid = (String)((object[])((MenuItem)sender).Tag)[4];
+            String iid = (String)((object[])((MenuItem)sender).Tag)[5];
+            AudioSessionControl audsess = (AudioSessionControl)((object[])((MenuItem)sender).Tag)[6];
 
             Console.WriteLine(String.Format("Assigning fader {0} to {1} - {2}", fader, proc, name));
-            assignments[fader] = new Assignment(proc, name, pid, aType);
+            assignments[fader] = new Assignment(proc, name, pid, aType, sid, iid, audsess);
         }
-
+        
         protected override void OnLoad(EventArgs e)
         {
             Visible = false; // Hide form window.
@@ -167,7 +293,7 @@ namespace NK2Tray
         }
 
         private void OnExit(object sender, EventArgs e) => Application.Exit();
-
+        /*
         public void EnumerateDevices()
         {
             for (int device = 0; device < MidiIn.NumberOfDevices; device++)
@@ -181,6 +307,7 @@ namespace NK2Tray
                 Console.WriteLine(MidiOut.DeviceInfo(device).ProductName);
             }
         }
+        */
 
         public void ListenForMidi()
         {
@@ -206,12 +333,14 @@ namespace NK2Tray
                 {
                     //Console.Write("Fader event; ");
                     int fader = (int)me.Controller;
-                    if (fader < 8 && assignments[fader].process != null)
+                    if (fader < 8 && assignments[fader].processName != null)
                     {
+                        Assignment assignment = assignments[fader];
                         //Console.Write("Got assignment " + fader + "; " + assignments[fader].windowName + "; ");
-                        if (assignments[fader].aType == AssignmentType.Process)
-                            VolumeMixer.SetApplicationVolume(assignments[fader].pid, me.ControllerValue / 127f * 100f);
-                        else if (assignments[fader].aType == AssignmentType.Master)
+                        if (assignment.aType == AssignmentType.Process)
+                            //VolumeMixer.SetApplicationVolume(assignments[fader].pid, me.ControllerValue / 127f * 100f);
+                            assignment.audioSession.SimpleAudioVolume.Volume = me.ControllerValue / 127f;
+                        else if (assignment.aType == AssignmentType.Master)
                         {
                             float vol = me.ControllerValue / 127f * 100f;
                             Console.WriteLine(String.Format("Setting master volume to: {0} {1}", vol, me.ControllerValue));
@@ -224,5 +353,6 @@ namespace NK2Tray
                 }
             }
         }
+        
     }
 }
