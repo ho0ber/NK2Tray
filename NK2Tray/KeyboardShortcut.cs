e32c5e8a36cd4703c6a856082f8ed5cd190db65a
Lukas <>
2019-12-10 22:10:40 +0100

    added shortcut class

---
+++ NK2Tray/KeyboardShortcut.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+    
+namespace NK2Tray
+{
+    class KeyboardShortcut
+    {
+        String shortcutString;
+        KeyboardShortcut(String shortcut)
+        {
+            shortcutString = shortcut;
+        }
+
+        void execute()
+        {
+            System.Windows.Forms.SendKeys.Send(shortcutString);
+            
+            //{ENTER}
+            //https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.sendkeys?view=netframework-4.8
+        }
+    }
+}
